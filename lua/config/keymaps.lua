ade7c22f89f277e9683bb1b2eb0589be18398e93
Danilo Freire <50254168+DaniloVFreire@users.noreply.github.com>
2024-09-14 11:12:28 -0300

    add leetcode plugin and disable cut and delete yank behaviour

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,15 @@
 
 -- Controversial mapping
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Disable yank on delete for common commands
+vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "c", '"_c', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "D", '"_D', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "X", '"_X', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "s", '"_s', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "S", '"_S', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "cc", '"_cc', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "C", '"_C', { noremap = true, silent = true })

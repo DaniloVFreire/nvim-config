0a74e165b3b738724022c27942852b9809e00a82
Danilo Freire <danilo.v.estudo@gmail.co>
2024-09-14 16:44:24 -0300

    add web icons

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -11,5 +11,14 @@ return {
     "rcarriga/nvim-notify",
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  opts = {
+    ---@type lc.lang
+    ---Avalible langs: cpp, java, python, python3, c, csharp, javascript,
+    ---typescript, php, swift, kotlin, dart, golang, ruby, scala, rust,
+    ---racket, erlang, elixir, bash
+    lang = "cpp",
+    cache = {
+      update_interval = 60 * 60 * 24 * 2, ---@type integer 2 days
+    },
+  },
 }
